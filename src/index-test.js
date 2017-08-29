79e6d301f56f75703aba261078d04989dabbafa4
Damien Clarke <dxinteractive@gmail.com>
2017-08-30 08:41:16 +1000

    Avoid tests and coverage for now

--- src/index-test.js
+++ src/index-test.js
@@ -1,8 +1,6 @@
-import React from 'react';
 import test from 'ava';
-// import parcels from './index';
+//import parcels from './index';
 
-// test('parcels does the right thing', tt => {
-//     tt.is(parcels(123), 246, 'parcels should add 123');
-//     tt.is(parcels(), 123, 'parcels should return 123 when given no arguments');
-// });
+test('this test should pass', tt => {
+    tt.is(3, 3, 'this test should pass');
+});
