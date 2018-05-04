a7da61c1323114d20407b2b32693907ce1b591aa
Damien Clarke <dxinteractive@gmail.com>
2018-05-04 16:10:56 +1000

    Add pure parcel

--- packages/parcels-react/src/PureParcel.jsx
+++ packages/parcels-react/src/PureParcel.jsx
@@ -1,36 +1,24 @@
 // @flow
 import React from 'react';
 import type {Element} from 'react';
-import PropTypes from 'prop-types';
 import Parcel from 'parcels';
 
 import shallowEquals from 'unmutable/lib/shallowEquals';
-import del from 'unmutable/lib/delete';
-import update from 'unmutable/lib/update';
-import pipe from 'unmutable/lib/util/pipe';
+
+type RenderFunction = (parcel: Parcel) => Element<*>;
 
 type Props = {
-    element: *,
-    render: (parcel: Parcel) => Element<*>,
+    children: RenderFunction,
+    forceUpdate?: Array<*>,
     parcel: Parcel
 };
 
 export default class PureParcel extends React.Component<Props> {
-    static defaultProps = {
-        element: "div"
-    };
-
-    static propTypes = {
-        element: PropTypes.string,
-        parcel: PropTypes.instanceOf(Parcel).isRequired,
-        render: PropTypes.func.isRequired
-    };
-
     shouldComponentUpdate(nextProps: Object): boolean {
-        let withParcelValue = pipe(
-            del("render"),
-            update("parcel", parcel => parcel && parcel.value())
-        );
+        let withParcelValue = ({parcel, forceUpdate = []}) => [
+            parcel.value(),
+            ...forceUpdate
+        ];
 
         let aa = withParcelValue(this.props);
         let bb = withParcelValue(nextProps);
@@ -38,16 +26,7 @@ export default class PureParcel extends React.Component<Props> {
     }
 
     render(): Element<*> {
-        let {
-            element,
-            render,
-            parcel,
-            ...rest
-        } = this.props;
-
-        let Component = element;
-        return <Component {...rest} >
-            {render(parcel)}
-        </Component>;
+        let {children, parcel} = this.props;
+        return children(parcel);
     }
 }
--- packages/parcels-react/src/index.js
+++ packages/parcels-react/src/index.js
@@ -1,3 +1,8 @@
 // @flow
 import parcels from 'parcels';
+import PureParcel from './PureParcel';
 export default parcels;
+
+export {
+    PureParcel
+};
