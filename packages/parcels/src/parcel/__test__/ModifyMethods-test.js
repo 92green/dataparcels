4dae354bdce1522dee971ded2990f94cb88ae9a4
Damien Clarke <dxinteractive@gmail.com>
2018-05-09 00:10:37 +1000

    Move top level methods back to where they started

--- packages/parcels/src/parcel/__test__/ModifyMethods-test.js
+++ packages/parcels/src/parcel/__test__/ModifyMethods-test.js
@@ -230,3 +230,22 @@ test('Parcel should addModifier with typed match', (tt: Object) => {
     tt.is(456, parcel.get('def').value(), "def parcel value proves that modifier has NOT been applied");
     tt.deepEqual([4,5,6,999], parcel.get('mno').value(), "mno parcel value proves that modifier has been applied");
 });
+
+test('Parcel should addPreModifier', (tt: Object) => {
+    tt.plan(4);
+
+    var data = {
+        value: 123,
+        handleChange: (parcel) => {
+            tt.is(parcel.id(), "~mv", "id() of handleChange parcel proves that preModifier have been applied already");
+            tt.is(parcel.value(), 457, "handleChange parcel value proves that modifier has been applied");
+        }
+    };
+
+    let parcel = new Parcel(data)
+        .addPreModifier((parcel) => parcel.modifyValue(ii => ii + 1));
+
+    tt.is("~mv", parcel.id(), "id() of constructed parcel proves that preModifier have been applied already");
+    tt.is(124, parcel.value(), "constructed parcel value proves that modifier has been applied");
+    parcel.onChange(456);
+});
