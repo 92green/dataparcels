1a30db9c88a055d1370107855b940bd5916cf54f
Damien Clarke <dxinteractive@gmail.com>
2018-05-22 13:44:43 +1000

    Export types

--- packages/parcels/src/index.js
+++ packages/parcels/src/index.js
@@ -1,3 +1,13 @@
 // @flow
 import Parcel from './parcel/Parcel';
 export default Parcel;
+
+export type {ParcelData} from './types/Types';
+export type {PartialParcelData} from './types/Types';
+export type {ParcelConfig} from './types/Types';
+export type {ParcelConfigInternal} from './types/Types';
+export type {CreateParcelConfigType} from './types/Types';
+export type {ModifierFunction} from './types/Types';
+export type {ModifierObject} from './types/Types';
+export type {Key} from './types/Types';
+export type {Index} from './types/Types';
