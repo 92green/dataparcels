af53f177e2aa09326522ccd72567c6c21447a819
Damien Clarke <dxinteractive@gmail.com>
2018-05-30 10:43:15 +1000

    Add debounce to ParcelPure

--- packages/parcels-docs/src/examples/02-pureRendering.jsx
+++ packages/parcels-docs/src/examples/02-pureRendering.jsx
@@ -7,11 +7,13 @@ const desc = `
 
 Before we go any further, if you're using parcels with React you'll care about render performance. By default all parcels in the tree will always re-render. This can be bad for render performance. The \`PureParcel\` component will only re-render when the \`parcel\` passed into it changes its data*.
 
-The colors in the example below will change on each re-render to demonstrate which elements are being re-rendered. You can turn this on by passing \`debugRender: true\` to your Parcel's config.
+The colors in the example below will change on each re-render to demonstrate which elements are being re-rendered. You can turn this on in your own apps by passing \`debugRender: true\` to your Parcel's config.
+
+\`ParcelPure\` can also debounce your changes to keep your text inputs feeling snappy and responsive. Pass it a \`debounce\` prop with the number of milliseconds you want to debounce.
 
 **Using PureParcel is VERY recommended.**
 
-* *You can force a \`PureParcel\` to update in response to other prop changes by giving it a prop of \`forceUpdate\`. This should be an array. Each element in the array will be checked for strict equality on \`shouldComponentUpdate\`, and the \`PureParcel\` will update if any of the elements have changed.*
+* *You can force a \`PureParcel\` to update in response to other prop changes by giving it a prop of \`forceUpdate\`. This should be an array. Each element in the array will be checked for strict equality on \`shouldComponentUpdate\`, and the \`PureParcel\` will update if any of the array elements have changed.*
 `;
 
 export default class ExamplePureRendering extends React.Component {
@@ -61,9 +63,9 @@ export default class ExamplePureRendering extends React.Component {
                 </div>}
             </PureParcel>
 
-            <PureParcel parcel={person.getIn(['address', 'city'])} debounce={100}>
+            <PureParcel parcel={person.getIn(['address', 'city'])} debounce={300}>
                 {(city) => <div>
-                    <label className="Label">city (with 100ms debounce)</label>
+                    <label className="Label">city (with 300ms debounce)</label>
                     <input className="Input" type="text" {...city.spreadDOM()} />
                 </div>}
             </PureParcel>
--- packages/parcels-react/src/PureParcel.jsx
+++ packages/parcels-react/src/PureParcel.jsx
@@ -9,16 +9,39 @@ type RenderFunction = (parcel: Parcel) => Node;
 
 type Props = {
     children: RenderFunction,
+    debounce?: number,
     forceUpdate?: Array<*>,
     parcel: Parcel
 };
 
-export default class PureParcel extends React.Component<Props> {
+type State = {
+    parcel: Parcel
+};
 
-    shouldComponentUpdate(nextProps: Object): boolean {
-        let parcelDataChanged: boolean = !this.props.parcel.equals(nextProps.parcel);
+export default class PureParcel extends React.Component<Props, State> {
+
+    changeCount: number = 0;
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            parcel: props.parcel
+        };
+    }
+
+    shouldComponentUpdate(nextProps: Props, nextState: State): boolean {
+        let parcelDataChanged: boolean = !this.state.parcel.equals(nextState.parcel);
         let forceUpdateChanged: boolean = !shallowEquals(this.props.forceUpdate || [])(nextProps.forceUpdate || []);
         return parcelDataChanged || forceUpdateChanged;
+    }
+
+    componentWillReceiveProps(nextProps: Object) {
+        let {parcel} = nextProps;
+        if(!this.props.parcel.equals(parcel)) {
+            this.setState({
+                parcel
+            });
+        }
     }
 
     debugRenderStyle: Function = (): Object => {
@@ -31,7 +54,29 @@ export default class PureParcel extends React.Component<Props> {
     };
 
     render(): Node {
-        let {children, parcel} = this.props;
+        let {
+            children,
+            debounce = 0
+        } = this.props;
+
+        let parcel = this.state.parcel
+            .modifyChange(({parcel, continueChange, newParcelData}: Object) => {
+                let parcelData = newParcelData();
+                this.setState({
+                    parcel: parcel._create({
+                        parcelData
+                    })
+                });
+
+                this.changeCount++;
+                let originalChangeCount = this.changeCount;
+                setTimeout(() => {
+                    if(originalChangeCount === this.changeCount) {
+                        continueChange();
+                    }
+                }, debounce);
+            });
+
         let element = children(parcel);
         if(parcel._treeshare.getDebugRender()) {
             return <div style={this.debugRenderStyle()}>{element}</div>;
