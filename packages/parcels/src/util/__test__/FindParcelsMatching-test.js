0398eb33ae1c9ffa1dc680596aceff40929df738
Damien Clarke <dxinteractive@gmail.com>
2018-06-14 14:27:42 +1000

    100% coverage on FindParcelsMatching

--- packages/parcels/src/util/__test__/FindParcelsMatching-test.js
+++ packages/parcels/src/util/__test__/FindParcelsMatching-test.js
@@ -19,6 +19,7 @@ test(`FindParcelsMatching() should return all parcels matching the match string 
 
     tt.deepEqual(["abc"], FindParcelsMatching(p, "abc").map(ii => ii.path().join(".")));
     tt.deepEqual(["abc.def"], FindParcelsMatching(p, "abc.def").map(ii => ii.path().join(".")));
+    tt.deepEqual([], FindParcelsMatching(p, "abc.def.toofar").map(ii => ii.path().join(".")));
     tt.deepEqual([], FindParcelsMatching(p, "abc.woo").map(ii => ii.path().join(".")));
     tt.deepEqual([], FindParcelsMatching(p, "asdf%.kasd.asdasd.asd").map(ii => ii.path().join(".")));
     tt.deepEqual(["abc.def", "abc.ghi"], FindParcelsMatching(p, "abc.*").map(ii => ii.path().join(".")));
