7ff68e6ac1bf439cf4814491c8c100e4555fdf0a
Damien Clarke <dxinteractive@gmail.com>
2018-07-24 09:57:01 +1000

    Make insertBefore() cope with index wrapping, and if key doesnt exist

--- packages/parcels/src/parcelData/__test__/insertBefore-test.js
+++ packages/parcels/src/parcelData/__test__/insertBefore-test.js
@@ -22,8 +22,10 @@ test('insertBefore should work', (tt: Object) => {
         ]
     };
 
-
-    tt.deepEqual(expectedParcelData, insertBefore(1, {value: 4})(parcelData));
+    tt.deepEqual(expectedParcelData, insertBefore(1, {value: 4})(parcelData), 'should work with in range number');
+    tt.deepEqual(expectedParcelData, insertBefore(-2, {value: 4})(parcelData), 'should work with in negative number');
+    tt.deepEqual(expectedParcelData, insertBefore(4, {value: 4})(parcelData), 'should work with positive wrapped number');
+    tt.deepEqual(expectedParcelData, insertBefore(-5, {value: 4})(parcelData), 'should work with negative wrapped number');
 });
 
 test('insertBefore should work with hashKey', (tt: Object) => {
@@ -48,4 +50,17 @@ test('insertBefore should work with hashKey', (tt: Object) => {
 
 
     tt.deepEqual(expectedParcelData, insertBefore("#b", {value: 4})(parcelData));
+});
+
+test('insertBefore should do nothing with non-existent hashKey', (tt: Object) => {
+    let parcelData = {
+        value: [1,2,3],
+        child: [
+            {key:"#a"},
+            {key:"#b"},
+            {key:"#c"}
+        ]
+    };
+
+    tt.deepEqual(parcelData, insertBefore("#z", {value: 4})(parcelData));
 });
