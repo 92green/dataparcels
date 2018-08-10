1f5ab1910ceeeec6345b01b584619fe37849844e
Damien Clarke <dxinteractive@gmail.com>
2018-08-10 10:46:31 +1000

    Make getting started page narrower

--- packages/dataparcels-docs/src/pages/getting-started.js
+++ packages/dataparcels-docs/src/pages/getting-started.js
@@ -1,11 +1,11 @@
 // @flow
 import type {Node} from 'react';
 import React from 'react';
-import {Box, Text, Typography} from 'dcme-style';
+import {Wrapper, Text, Typography} from 'dcme-style';
 import GettingStartedMarkdown from 'pages/getting-started.md';
 
-export default () => <Box>
+export default () => <Wrapper modifier="medium">
     <Typography>
         <GettingStartedMarkdown />
     </Typography>
-</Box>;
+</Wrapper>;
