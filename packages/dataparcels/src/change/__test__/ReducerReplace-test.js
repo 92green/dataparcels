7220be6a0048086d309c559ea151e7ba7bb4388f
Damien Clarke <damienclarke@Damiens-MacBook-Pro.local>
2018-09-11 17:32:27 +1000

    Remove tests that shouldnt exist

--- packages/dataparcels/src/change/__test__/ReducerReplace-test.js
+++ packages/dataparcels/src/change/__test__/ReducerReplace-test.js
@@ -155,66 +155,6 @@ test('Reducer should replace with keyPath of 1 element', () => {
     expect(Reducer(data, action)).toEqual(expectedData);
 });
 
-
-test('Reducer should clear child from replace key', () => {
-    var data = {
-        value: {
-            a: {
-                c: 123
-            },
-            b: 2
-        },
-        meta: {
-            abc: 123
-        },
-        key: "^",
-        child: {
-            a: {
-                key: "a",
-                child: {
-                    c: {
-                        key: "c",
-                        meta: {
-                            def: 456
-                        }
-                    }
-                }
-            },
-            b: {
-                key: "b"
-            }
-        }
-    };
-    var action = new Action({
-        type: "replace",
-        keyPath: ["a"],
-        payload: {
-            value: 3
-        }
-    });
-
-    var expectedData = {
-        value: {
-            a: 3,
-            b: 2
-        },
-        meta: {
-            abc: 123
-        },
-        key: "^",
-        child: {
-            a: {
-                key: "a"
-            },
-            b: {
-                key: "b"
-            }
-        }
-    };
-
-    expect(Reducer(data, action)).toEqual(expectedData);
-});
-
 test('Reducer should replace with keyPath of 2 elements', () => {
     var data = {
         value: {
@@ -266,58 +206,6 @@ test('Reducer should replace with keyPath of 2 elements', () => {
                 key: "b"
             }
         }
-    };
-
-    expect(Reducer(data, action)).toEqual(expectedData);
-});
-
-test('Reducer should replace with keyPath of 2 elements on arrays', () => {
-    var data = {
-        value: [
-            [],
-            [1,2,3]
-        ],
-        meta: {
-            abc: 123
-        },
-        key: "^"
-    };
-    var action = new Action({
-        type: "replace",
-        keyPath: ["#b", "#c"],
-        payload: {
-            value: [4]
-        }
-    });
-
-    var expectedData = {
-        value: [
-            [],
-            [1,2,[4]]
-        ],
-        meta: {
-            abc: 123
-        },
-        key: "^",
-        child: [
-            {
-                key: "#a"
-            },
-            {
-                key: "#b",
-                child: [
-                    {
-                        key: "#a"
-                    },
-                    {
-                        key: "#b"
-                    },
-                    {
-                        key: "#c"
-                    },
-                ]
-            }
-        ]
     };
 
     expect(Reducer(data, action)).toEqual(expectedData);
