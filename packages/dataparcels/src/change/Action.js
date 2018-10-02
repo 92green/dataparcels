b2c5e3ab5e406856a28967caafe067b91f7b4af8
Damien Clarke <dxinteractive@gmail.com>
2018-10-03 08:38:09 +1000

    feat: Add keyPathModifiers to Action

--- packages/dataparcels/src/change/Action.js
+++ packages/dataparcels/src/change/Action.js
@@ -1,29 +1,77 @@
 // @flow
 import type {Key} from '../types/Types';
 
+import has from 'unmutable/lib/has';
+import set from 'unmutable/lib/set';
+import unshift from 'unmutable/lib/unshift';
+
 type ActionData = {
     type?: string,
     payload?: Object,
     keyPath?: Array<Key>
 };
 
+type CreateActionData = {
+    type?: string,
+    payload?: Object,
+    keyPath?: Array<Key>,
+    keyPathModifiers?: Array<KeyPathModifiers>
+};
+
+type KeyPathModifiers = {
+    key?: Key,
+    pre?: Function,
+    post?: Function
+};
+
 export default class Action {
     type: string = "";
     payload: Object = {};
     keyPath: Array<Key> = [];
+    keyPathModifiers: Array<KeyPathModifiers> = [];
 
-    constructor(actionData: ActionData = {}) {
-        this.type = actionData.type || this.type;
-        this.payload = actionData.payload || this.payload;
-        this.keyPath = actionData.keyPath || this.keyPath;
+    constructor({type, payload, keyPath}: ActionData = {}) {
+        this.type = type || this.type;
+        this.payload = payload || this.payload;
+        this.keyPath = keyPath || this.keyPath;
+
+        if(keyPath) {
+            this.keyPathModifiers = keyPath.map(key => ({key}));
+        }
     }
 
-    _unget = (key: Key): Action => {
-        let {type, payload, keyPath} = this;
+    _create = (create: CreateActionData): Action => {
         return new Action({
-            type,
-            payload,
-            keyPath: [key, ...keyPath]
+            ...this.toJS(),
+            ...create
+        });
+    };
+
+    _updateFirstKeyPathModifier = (obj: *, keyPathModifiers: Array<KeyPathModifiers>): Array<KeyPathModifiers> => {
+        let first = keyPathModifiers[0];
+        let fn = (!first || !has("key")(first))
+            ? unshift(obj)
+            : set(0, {...obj, ...first});
+
+        return fn(keyPathModifiers);
+    };
+
+    _unget = (key: Key): Action => {
+        return this._create({
+            keyPath: unshift(key)(this.keyPath),
+            keyPathModifiers: this._updateFirstKeyPathModifier({key}, this.keyPathModifiers)
+        });
+    };
+
+    _addPre = (pre: Function) => {
+        return this._create({
+            keyPathModifiers: this._updateFirstKeyPathModifier({pre}, this.keyPathModifiers)
+        });
+    };
+
+    _addPost = (post: Function) => {
+        return this._create({
+            keyPathModifiers: this._updateFirstKeyPathModifier({post}, this.keyPathModifiers)
         });
     };
 
@@ -40,7 +88,7 @@ export default class Action {
     };
 
     toJS = (): ActionData => {
-        let {type, payload, keyPath} = this;
-        return {type, payload, keyPath};
+        let {type, payload, keyPath, keyPathModifiers} = this;
+        return {type, payload, keyPath, keyPathModifiers};
     };
 }
