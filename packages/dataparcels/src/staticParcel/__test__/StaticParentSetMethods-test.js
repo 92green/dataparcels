fa1c71c076be1cac7069457501dd826a6114f0c7
Damien Clarke <dxinteractive@gmail.com>
2019-01-13 21:09:51 +1100

    add: add static parcel

---
+++ packages/dataparcels/src/staticParcel/__test__/StaticParentSetMethods-test.js
@@ -0,0 +1,236 @@
+// @flow
+import StaticParcel from '../StaticParcel';
+import TestValidateValueUpdater from '../../util/__test__/TestValidateValueUpdater-testUtil';
+
+test('StaticParcels set(key) should work', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: {
+            a: 1,
+            b: 4
+        },
+        child: {
+            a: {key: "a"},
+            b: {key: "b"}
+        }
+    });
+
+    let expectedData = {
+        value: {
+            a: 456,
+            b: 4
+        },
+        child: {
+            a: {
+                child: undefined,
+                key: "a"
+            },
+            b: {
+                key: "b"
+            }
+        }
+    };
+
+    expect(staticParcel.set('a', 456).data).toEqual(expectedData);
+});
+
+test('StaticParcels setIn(keyPath) should work', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: {
+            a: {
+                b: 123
+            },
+            c: 456
+        }
+    });
+
+    let expectedData = {
+        value: {
+            a: {
+                b: 456
+            },
+            c: 456
+        },
+        child: {
+            a: {
+                child: {
+                    b: {
+                        child: undefined,
+                        key: "b"
+                    }
+                },
+                key: "a"
+            },
+            c: {
+                key: "c"
+            }
+        }
+    };
+
+    expect(staticParcel.setIn(['a', 'b'], 456).data).toEqual(expectedData);
+});
+
+test('StaticParcels delete(key) should work', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: {
+            a: 1,
+            b: 4
+        },
+        child: {
+            a: {key: "a"},
+            b: {key: "b"}
+        }
+    });
+
+    let expectedData = {
+        value: {
+            b: 4
+        },
+        child: {
+            b: {
+                key: "b"
+            }
+        }
+    };
+
+    expect(staticParcel.delete('a').data).toEqual(expectedData);
+});
+
+test('StaticParcels deleteIn(keyPath) should work', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: {
+            a: {
+                b: 123
+            },
+            c: 456
+        }
+    });
+
+    let expectedData = {
+        value: {
+            a: {},
+            c: 456
+        },
+        child: {
+            a: {
+                child: {},
+                key: "a"
+            },
+            c: {
+                key: "c"
+            }
+        }
+    };
+
+    expect(staticParcel.deleteIn(['a', 'b']).data).toEqual(expectedData);
+});
+
+test('StaticParcels update(key) should work', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: {
+            a: 1,
+            b: 4
+        }
+    });
+
+    let expectedData = {
+        value: {
+            a: 2,
+            b: 4
+        },
+        child: {
+            a: {
+                child: undefined,
+                key: "a"
+            },
+            b: {
+                child: undefined,
+                key: "b"
+            }
+        }
+    };
+
+    expect(staticParcel.update('a', ii => ii + 1).data).toEqual(expectedData);
+});
+
+test('StaticParcels update(key) should validate value updater', () => {
+    TestValidateValueUpdater(
+        expect,
+        (value, updater) => new StaticParcel({
+            abc: value
+        }).update('abc', updater)
+    );
+});
+
+test('StaticParcels updateIn(keyPath) should work', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: {
+            a: {
+                b: 123
+            }
+        }
+    });
+
+    let expectedData = {
+        value: {
+            a: {
+                b: 124
+            }
+        },
+        child: {
+            a: {
+                child: {
+                    b: {
+                        child: undefined,
+                        key: "b"
+                    }
+                },
+                key: "a"
+            }
+        }
+    };
+
+    expect(staticParcel.updateIn(['a', 'b'], ii => ii + 1).data).toEqual(expectedData);
+});
+
+test('StaticParcels updateIn(keyPath) should validate value updater', () => {
+    TestValidateValueUpdater(
+        expect,
+        (value, updater) => new StaticParcel({
+            a: {
+                b: value
+            }
+        }).updateIn(['a', 'b'], updater)
+    );
+});
+
+test('StaticParcels updateDeep(key) should work', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: {
+            abc: [1,2,3]
+        }
+    });
+
+    let expectedValue = {
+        abc: [1,2,3,4]
+    };
+
+    expect(staticParcel.updateDeep('abc', staticParcel => staticParcel.push(4)).value).toEqual(expectedValue);
+});
+
+test('StaticParcels updateInDeep(keyPath) should work', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: {
+            abc: {
+                def: [1,2,3]
+            }
+        }
+    });
+
+    let expectedValue = {
+        abc: {
+            def: [1,2,3,4]
+        }
+    };
+
+    expect(staticParcel.updateInDeep(['abc', 'def'], staticParcel => staticParcel.push(4)).value).toEqual(expectedValue);
+});
---
+++ packages/dataparcels/src/staticParcel/__test__/StaticSetMethods-test.js
@@ -0,0 +1,230 @@
+// @flow
+import StaticParcel from '../StaticParcel';
+import TestValidateValueUpdater from '../../util/__test__/TestValidateValueUpdater-testUtil';
+
+test('StaticParcels set() should work', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: {
+            a: 1,
+            b: 4
+        },
+        child: {
+            a: {key: "a"},
+            b: {key: "b"}
+        }
+    });
+
+    let expectedData = {
+        value: 456
+    };
+
+    expect(staticParcel.set(456).data).toEqual(expectedData);
+});
+
+test('StaticParcels setMeta(partialMeta) should work', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: 123,
+        meta: {
+            abc: 123
+        }
+    });
+
+    let expectedData = {
+        value: 123,
+        meta: {
+            abc: 123,
+            def: 456
+        }
+    };
+
+    expect(staticParcel.setMeta({def: 456}).data).toEqual(expectedData);
+});
+
+test('StaticParcels setMeta(updater) should work', () => {
+    let updater = jest.fn(meta => ({def: 456}));
+
+    let staticParcel = StaticParcel.fromData({
+        value: 123,
+        meta: {
+            abc: 123
+        }
+    });
+
+    let expectedData = {
+        value: 123,
+        meta: {
+            abc: 123,
+            def: 456
+        }
+    };
+
+    let {data} = staticParcel.setMeta(updater);
+
+    expect(updater.mock.calls[0][0]).toEqual({
+        abc: 123
+    });
+
+    expect(data).toEqual(expectedData);
+});
+
+test('StaticParcels update() should work', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: 123
+    });
+
+    let expectedData = {
+        value: 124
+    };
+
+    expect(staticParcel.update(ii => ii + 1).data).toEqual(expectedData);
+});
+
+test('StaticParcels update() should validate value updater', () => {
+    TestValidateValueUpdater(
+        expect,
+        (value, updater) => new StaticParcel(value).update(updater)
+    );
+});
+
+test('StaticParcels updateDeep() should work with returned StaticParcel', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: 123,
+        meta: {
+            abc: 789
+        },
+        key: "z"
+    });
+
+    let expectedData = {
+        value: 456,
+        meta: {
+            abc: 789
+        },
+        key: "z"
+    };
+
+    expect(staticParcel.updateDeep(staticParcel => staticParcel.set(456)).data).toEqual(expectedData);
+});
+
+test('StaticParcels updateDeep() should work with returned primitive', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: 123,
+        meta: {
+            abc: 789
+        },
+        key: "z"
+    });
+
+    let expectedData = {
+        value: 456,
+        meta: {
+            abc: 789
+        },
+        key: "z"
+    };
+
+    expect(staticParcel.updateDeep(() => 456).data).toEqual(expectedData);
+});
+
+test('StaticParcels updateDeep() should work with returned parent value', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: [1,2,3],
+        meta: {
+            abc: 789
+        },
+        key: "z"
+    });
+
+    let expectedData = {
+        value: [2,3,4],
+        meta: {
+            abc: 789
+        },
+        child: [
+            {key: "#a", child: undefined},
+            {key: "#b", child: undefined},
+            {key: "#c", child: undefined}
+        ],
+        key: "z"
+    };
+
+    let {data} = staticParcel.updateDeep((staticParcel) => {
+        return staticParcel
+            .children()
+            .map((child => child.update(value => value + 1)))
+    });
+
+    expect(data).toEqual(expectedData);
+});
+
+test('StaticParcels updateDeep() should work with returned parent value of different type', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: {
+            abc: 123,
+            def: 456
+        },
+        meta: {
+            abc: 789
+        },
+        key: "z"
+    });
+
+    let expectedData = {
+        value: [
+            123,
+            456
+        ],
+        meta: {
+            abc: 789
+        },
+        child: [
+            {key: "#a", child: undefined},
+            {key: "#b", child: undefined}
+        ],
+        key: "z"
+    };
+
+    let {data} = staticParcel.updateDeep((staticParcel) => staticParcel.toArray());
+
+    expect(data).toEqual(expectedData);
+});
+
+test('StaticParcels updateDeep() should retain childs keys', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: ["a","b","c","d"],
+        child: [
+            {key: "#a", child: undefined},
+            {key: "#b", child: undefined, meta: {abc: 123}},
+            {key: "#c", child: undefined},
+            {key: "#d", child: undefined}
+        ]
+    });
+
+    let expectedData = {
+        value: ["b","d"],
+        child: [
+            {key: "#b", child: undefined, meta: {abc: 123}},
+            {key: "#d", child: undefined}
+        ]
+    };
+
+    let {data} = staticParcel.updateDeep((staticParcel) => {
+        return staticParcel
+            .toArray()
+            .filter((value, key) => key % 2 === 1)
+    });
+
+    expect(data).toEqual(expectedData);
+});
+
+test('StaticParcels updateDeep() should throw error if non staticparcel is a child of the return value', () => {
+    let staticParcel = StaticParcel.fromData({
+        value: [123, 456]
+    });
+
+    expect(() => staticParcel.updateDeep((staticParcel) => {
+        let arr = staticParcel.toArray();
+        arr.push(789);
+        return arr;
+    })).toThrow('Every child value on a collection returned from a deep updater must be a StaticParcel');
+});
