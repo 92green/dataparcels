27fb9f2357a63b7d2cd25802d17829c719d70a37
Damien Clarke <dxinteractive@gmail.com>
2019-01-28 23:16:00 +1100

    refactor: remove unneeded error

--- packages/dataparcels/src/errors/Errors.js
+++ packages/dataparcels/src/errors/Errors.js
@@ -2,7 +2,6 @@
 
 export const ReadOnlyError = () => new Error(`This property is read-only`);
 export const ReducerInvalidActionError = (actionType: string) => new Error(`"${actionType}" is not a valid action`);
-export const UnsafeValueUpdaterError = () =>  new Error(`Value updaters can only pass collections through if the collection is unchanged by the updater. Consider using a deep updater method (e.g. <method name>Deep()) for updating collections.`);
+export const UnsafeValueUpdaterError = () =>  new Error(`Value updaters can only pass collections through if the collection is unchanged by the updater. Consider using a shape updater method (e.g. <method name>Shape()) for updating collections.`);
 export const ChangeRequestUnbasedError = () =>  new Error(`ChangeRequest data cannot be accessed before calling changeRequest._setBaseParcel()`);
 export const ShapeUpdaterNonShapeChildError = () =>  new Error(`Every child value on a collection returned from a shape updater must be a ParcelShape`);
-export const ShapeUpdaterUndefinedError = () =>  new Error(`ShapeUpdaterUndefinedError`);
