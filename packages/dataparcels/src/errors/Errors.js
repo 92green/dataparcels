50c1b8b7293712ff1b061c21d0c03fcd506789bd
Damien Clarke <dxinteractive@gmail.com>
2019-01-31 12:48:50 +1100

    add: remove value updater error in favour of warning

--- packages/dataparcels/src/errors/Errors.js
+++ packages/dataparcels/src/errors/Errors.js
@@ -2,6 +2,5 @@
 
 export const ReadOnlyError = () => new Error(`This property is read-only`);
 export const ReducerInvalidActionError = (actionType: string) => new Error(`"${actionType}" is not a valid action`);
-export const UnsafeValueUpdaterError = () =>  new Error(`Value updaters can only pass collections through if the collection is unchanged by the updater. Consider using a shape updater method (e.g. <method name>Shape()) for updating collections.`);
 export const ChangeRequestUnbasedError = () =>  new Error(`ChangeRequest data cannot be accessed before calling changeRequest._setBaseParcel()`);
 export const ShapeUpdaterNonShapeChildError = () =>  new Error(`Every child value on a collection returned from a shape updater must be a ParcelShape`);
