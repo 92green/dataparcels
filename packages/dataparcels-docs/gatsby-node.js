884cde1e2524fe66e5f566d0026fe9e22cb24f19
Damien Clarke <dxinteractive@gmail.com>
2019-04-23 15:15:14 +1000

    docs: kill jest workers to fix netlify deploys
    kill all jest child processes that hang around after build,
    which stop netlify from recognising that build is complete
    this likely happens due to the usage of setInterval within code that
    ends up in the gatsby site

--- packages/dataparcels-docs/gatsby-node.js
+++ packages/dataparcels-docs/gatsby-node.js
@@ -30,3 +30,16 @@ exports.onCreateWebpackConfig = ({
 }
 
 console.log("...gatsby-node.js is valid");
+
+// kill all jest child processes that hang around after build,
+// which stop netlify from recognising that build is complete
+// this likely happens due to the usage of setInterval within code that
+// ends up in the gatsby site
+
+const ChildProcess = require('child_process');
+
+exports.onPostBuild = () => {
+    console.log("Killing jest...");
+    ChildProcess.execSync("ps aux | grep jest | grep -v grep | awk '{print $2}' | xargs kill");
+    console.log("Jest is dead");
+};
