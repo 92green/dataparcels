8a45d0562fd87faaa3a67caf18baab80bc5537ff
Damien Clarke <dxinteractive@gmail.com>
2019-05-21 23:07:28 +1000

    refactor: rename internal ApplyModifyBeforeChange func

--- packages/react-dataparcels/src/util/ApplyModifyBeforeUpdate.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import type {ParcelValueUpdater} from 'dataparcels';
-
-import compose from 'unmutable/compose';
-
-export default (modifyBeforeUpdate: Array<ParcelValueUpdater>) => compose(
-    ...modifyBeforeUpdate.map((fn) => parcel => parcel.modifyUp(fn))
-);
