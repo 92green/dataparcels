12b798e7be9fc876caa1cfe7b65712a1b4ac6f82
Damien Clarke <dxinteractive@gmail.com>
2019-06-01 19:17:14 +1000

    docs: add data sync example of basic form submission

---
+++ packages/dataparcels-docs/src/examples/SubmitButtonOnChange.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {useRef} from 'react';
+import {useState} from 'react';
+import useParcelForm from 'react-dataparcels/useParcelForm';
+import ParcelBoundary from 'react-dataparcels/ParcelBoundary';
+import exampleFrame from 'component/exampleFrame';
+
+const initialValue = {
+    firstname: "",
+    lastname: ""
+};
+
+export default function SignUpForm(props) {
+
+    let [requestState, setRequestState] = useState("idle");
+    let rejectRef = useRef(() => {});
+
+    let saveMyData = () => new Promise((resolve, reject) => {
+        setRequestState("pending...");
+
+        let timeout = setTimeout(() => {
+            setRequestState("resolved");
+            resolve();
+        }, 2000);
+
+        rejectRef.current = () => {
+            setRequestState("rejected");
+            clearTimeout(timeout);
+            reject();
+        };
+    });
+
+    let [personParcel, personParcelBuffer] = useParcelForm({
+        value: initialValue,
+        onChange: (parcel) => saveMyData(parcel.value)
+        // ^ returns a promise
+    });
+
+    let personParcelState = personParcelBuffer._outerParcel;
+    return exampleFrame({personParcelState, personParcel}, <div>
+        <label>firstname</label>
+        <ParcelBoundary parcel={personParcel.get('firstname')}>
+            {(firstname) => <input type="text" {...firstname.spreadDOM()} />}
+        </ParcelBoundary>
+
+        <label>lastname</label>
+        <ParcelBoundary parcel={personParcel.get('lastname')}>
+            {(lastname) => <input type="text" {...lastname.spreadDOM()} />}
+        </ParcelBoundary>
+
+        <button onClick={() => personParcelBuffer.submit()}>Submit</button>
+
+        <p>Request state: <strong>{requestState}</strong>
+            {requestState === "pending..." && <button onClick={rejectRef.current}>reject</button>}
+        </p>
+    </div>);
+}
--- packages/dataparcels-docs/src/pages/data-synchronisation.js
+++ packages/dataparcels-docs/src/pages/data-synchronisation.js
@@ -8,7 +8,8 @@ export default () => <Layout>
     <ContentNav
         content={() => <DataSynchronisationMarkdown />}
         pageNav={[
-            '# Data synchronisation'
+            '# Data synchronisation',
+            'Saving data from a form'
         ]}
     />
 </Layout>;
