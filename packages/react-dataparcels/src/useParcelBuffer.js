8e60b333d919448e06c333ac5bfeea877c1f8fbe
Damien Clarke <dxinteractive@gmail.com>
2019-06-01 19:14:17 +1000

    refactor: make reset and submit after pushing actions to buffer, in case we allow submit actions to be bundled up in the same change request as data changes in future. this way, the changes will be submitted after they are applied

--- packages/react-dataparcels/src/useParcelBuffer.js
+++ packages/react-dataparcels/src/useParcelBuffer.js
@@ -133,11 +133,6 @@ export default (params: Params): Return => {
         const handleChange = (newParcel: Parcel, changeRequest: ChangeRequest) => {
             const {debounce, buffer = true, keepValue} = params;
 
-            // trigger buffer actions if meta says so
-            const {_submit, _reset} = newParcel.meta;
-            _submit && internalBuffer.submit();
-            _reset && internalBuffer.reset();
-
             // remove buffer actions meta from change request
             // and push any remaining change into the buffer
             let actions = changeRequest._actions.filter(removeInternalMeta);
@@ -145,19 +140,24 @@ export default (params: Params): Return => {
                 internalBuffer.push(changeRequest._create({actions}));
             }
 
-            if(!buffer) {
+            if(buffer) {
+                // if buffer is to be used, update inner parcel immediately
+                // and request a debounced submit if debounce is set
+                setInnerParcel(newParcel.pipe(applyModifiers));
+                debounce && debounceRelease();
+
+            } else {
                 // if no buffer, just propagate it immediately
                 internalBuffer.submit();
                 // if keepValue is true, the buffer is in change of its own state
                 // rather than waiting for new props containing the new value
                 keepValue && setInnerParcel(newParcel.pipe(applyModifiers));
-                return;
             }
 
-            // if buffer is to be used, update inner parcel immediately
-            // and request a debounced submit if debounce is set
-            setInnerParcel(newParcel.pipe(applyModifiers));
-            debounce && debounceRelease();
+            // trigger buffer actions if meta says so
+            const {_submit, _reset} = newParcel.meta;
+            _submit && internalBuffer.submit();
+            _reset && internalBuffer.reset();
         };
 
         const newOuterParcel = params.parcel;
