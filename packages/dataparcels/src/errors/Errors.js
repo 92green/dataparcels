2e2b13b1c918d2e607f7b1490280b44100428d2d
Damien Clarke <dxinteractive@gmail.com>
2020-04-02 16:43:04 +1100

    refactor: remove internal errors to save bundle size #2

--- packages/dataparcels/src/errors/Errors.js
+++ packages/dataparcels/src/errors/Errors.js
@@ -4,4 +4,3 @@ export const ParcelTypeMethodMismatch = (key: string, parcelType: string, path: 
     let suffix = Array.isArray(path) ? `(keyPath: [${path.join(', ')}]).` : ``;
     return new Error(`.${key}() is not a function. It can only be called on parcels of type "${parcelType}". ${suffix}`);
 };
-export const ChangeRequestNoPrevDataError = () =>  new Error(`ChangeRequest data cannot be accessed before setting changeRequest.prevData`);
