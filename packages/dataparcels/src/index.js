1e001a30670112736bb93f0e21c8fc0225634391
Damien Clarke <dxinteractive@gmail.com>
2020-05-13 17:06:24 +1000

    break: stop exporting flow types

--- packages/dataparcels/src/index.js
+++ packages/dataparcels/src/index.js
@@ -1,24 +1,3 @@
 // @flow
 import Parcel from './parcel/Parcel';
 export default Parcel;
-
-//
-// types
-//
-
-export type {ParcelData} from './types/Types';
-export type {ParcelConfig} from './types/Types';
-export type {ParcelConfigInternal} from './types/Types';
-export type {ParcelCreateConfigType} from './types/Types';
-export type {ParcelMeta} from './types/Types';
-
-export type {ParcelMapper} from './types/Types';
-export type {ParcelUpdater} from './types/Types';
-export type {ParcelValueUpdater} from './types/Types';
-
-export type {Key} from './types/Types';
-export type {Index} from './types/Types';
-export type {Property} from './types/Types';
-export type {ParentType} from './types/Types';
-
-export type {ContinueChainFunction} from './types/Types';
