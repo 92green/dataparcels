27c722cb093041d5daf19ff35c53fb0728dbcd3c
Damien Clarke <dxinteractive@gmail.com>
2020-05-13 17:23:29 +1000

    refactor: remove old parcelData unmutable wrapper files

--- packages/dataparcels/src/parcelData/isParentValue.js
+++
@@ -1,7 +0,0 @@
-// @flow
-import isValueObject from 'unmutable/lib/util/isValueObject';
-import isWriteable from 'unmutable/lib/util/isWriteable';
-
-export default (value: *): boolean => {
-    return isValueObject(value) && isWriteable(value);
-};
