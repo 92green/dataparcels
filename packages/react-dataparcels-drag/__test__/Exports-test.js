c2448cd3f432c153139a397c584d264e01a0679a
Damien Clarke <dxinteractive@gmail.com>
2020-05-18 21:22:00 +1000

    refactor: remove deprecated dataparcels-drag package

--- packages/react-dataparcels-drag/__test__/Exports-test.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-// dataparcels exports
-import Drag from '../src/index';
-import InternalDrag from '../src/Drag';
-
-test('index should export Drag', () => {
-    expect(Drag).toBe(InternalDrag);
-});
